7f4792cca4121e496b02ec2d58512c20a3a24af2
sara-mukesh-kumar <saramukeshkumar@gmail.com>
2026-04-07 16:51:24 +0530

    updated the not-found.jsx to remove the error while building the vercle

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import{ Button } from "@/components/ui/button";
-export default function NotFound(){
-    return(
-        <div className="flex flex-col items-center justify-center min-h-[100vh] px-4 text-center">
+export const dynamic = 'force-dynamic';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[100vh] px-4 text-center">
             <h1 className="text-6xl font-bold gradient-title mb-4">404</h1>
             <h2 className="text-2xl font-semibold md-4">Page Not Found</h2>
             <p className="text-gray-600 mb-8">
@@ -12,5 +14,5 @@ export default function NotFound(){
             <Button>Return Home</Button>
             </Link>
         </div>
-    )
+  );
 }
