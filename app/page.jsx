e475c7a586a6b99c7a91f7084a9304b1de04decf
sara-mukesh-kumar <saramukeshkumar@gmail.com>
2026-04-07 16:56:07 +0530

    updated the app/page.jsx to remove the error while building the vercle

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import HeroSection from "@/components/hero";
 import{ Accordion,AccordionItem,AccordionContent,AccordionTrigger } from "@/components/ui/accordion";
 import{ Button } from "@/components/ui/button";
